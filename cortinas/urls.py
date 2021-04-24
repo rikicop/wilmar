24694dc5498a7fa3f8a525ae2bac3053dd79ed8d
rikicop <ruperto1@protonmail.com>
2021-04-24 16:06:35 -0400

    download button

--- cortinas/urls.py
+++ cortinas/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('detalle/<str:pk>', views.product,name='detalle'),
+    path('download_file/',views.download_file, name='download_file')
 ]
--- cortinas/views.py
+++ cortinas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Product
+import mimetypes
 
 def home(request):
     prod = Product.objects.all()
@@ -10,3 +12,14 @@ def product(request, pk):
     prod = Product.objects.get(id=pk)
     #context = {'prod':prod}
     return render(request, 'detalle.html', {'prod':prod})
+
+def download_file(request):
+  
+    fl_path = 'wilmar_project/media/historical.xlsx'
+    filename = 'historical.xlsx'
+
+    fl = open(fl_path, 'rb')
+    mime_type, _ = mimetypes.guess_type(fl_path)
+    response = HttpResponse(fl, content_type=mime_type)
+    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    return response
